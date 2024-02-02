3879540fc1b39b5493a60a4ab0da12219fd0ad22
yogyy <yogimr261@gmail.com>
2024-02-02 23:59:51 +0700

    feat: add cors

---
+++ plugins/cors.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fp = require('fastify-plugin');
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('@fastify/cors'), instance => {
+    return (req, callback) => {
+      const corsOptions = {
+        // This is NOT recommended for production as it enables reflection exploits
+        origin: true,
+        credentials: true,
+      };
+
+      // do not include CORS headers for requests from localhost
+      if (/^localhost$/m.test(req.headers.origin)) {
+        corsOptions.origin = false;
+      }
+
+      // callback expects two parameters: error and options
+      callback(null, corsOptions);
+    };
+  });
+});
