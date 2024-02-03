4886b959838ee5a1785ae61564c212758a683673
yogyy <yogimr261@gmail.com>
2024-02-03 19:10:32 +0700

    chore: update root

--- routes/root.js
+++ routes/root.js
@@ -1,32 +1,53 @@
-'use strict';
+"use strict";
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', function (request, reply) {
-    return { code: 'hello world' };
+  fastify.get("/", function (_, reply) {
+    return reply.status(200).type("text/html").send(html);
   });
 
-  fastify.get('/ping', async (request, reply) => {
-    return reply.notFound();
-  });
-  fastify.get('/users/:id', async (req, reply) => {
-    // const connection = await fastify.mysql.getConnection();
-    // const [rows, fields] = await connection.query(
-    //   `SELECT * FROM users where id=${req.params.id}`
-    // );
-    // // `SELECT id, email, firstName, lastName FROM users where id=${req.params.id}`;
-    // if (!rows[0]) {
-    //   return { error: 'users not found' };
-    // }
-    // // console.log(rows[0].hash);
-    // connection.release();
-    // return rows[0];
-    fastify.mysql.query(
-      `SELECT id, email, firstName, lastName FROM users where id=${req.params.id}`,
-      function onResult(err, result) {
-        if (err) throw err;
-        reply.send(result);
-      }
-    );
-    return reply;
+  fastify.get("/ping", async (_, reply) => {
+    return reply.status(200).send("pong");
   });
 };
+
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css"
+    />
+    <title>Vercel + Fastify Hello World</title>
+    <meta
+      name="description"
+      content="This is a starter template for Vercel + Fastify."
+    />
+  </head>
+  <body>
+    <h1>Vercel + Fastify Hello World</h1>
+    <p>
+      This is a starter template for Vercel + Fastify. Requests are
+      rewritten from <code>/*</code> to <code>/api/*</code>, which runs
+      as a Vercel Function.
+    </p>
+    <p>
+        For example, here is the boilerplate code for this route:
+    </p>
+    <pre>
+<code>"use strict";
+
+module.exports = async function (fastify, opts) {
+  fastify.get("/", function (req, reply) {
+    return reply.status(200).type("text/html").send(html);
+  });
+
+  fastify.get("/ping", async (req, reply) => {
+    return reply.status(200).send("pong");
+  });
+};</code>
+  </body>
+</html>
+`;
